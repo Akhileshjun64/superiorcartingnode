71349de7a10052f85a4593f7c20c4b3e521d010c
akhileshjune64 <akhileshjun64@gmail.com>
2023-11-24 15:28:03 +0530

    db update

--- dbconnect.js
+++ dbconnect.js
@@ -4,11 +4,17 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    // await mongoose.connect(`${process.env.MONGODB_URI}/post`);
-    await mongoose.connect(`${process.env.MONGODB_URI_CLOUD}/post`);
+    const connectionString = process.env.MONGODB_URI_CLOUD;
 
+    await mongoose.connect(connectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      connectTimeoutMS: 30000, // 30 seconds
+      socketTimeoutMS: 30000, // 30 seconds
+    });
+
+    mongoose.set("strictQuery", false);
     console.log("Connected to MongoDB");
-    mongoose.set("strictQuery", false); // Add this line
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
   }
