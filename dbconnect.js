784464dd42329d980c369810f28c7dbadca119e8
akhileshjune64 <akhileshjun64@gmail.com>
2023-11-24 15:49:51 +0530

    db update

--- dbconnect.js
+++ dbconnect.js
@@ -7,8 +7,6 @@ const connectDB = async () => {
     const connectionString = process.env.MONGODB_URI_CLOUD;
 
     await mongoose.connect(connectionString, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       connectTimeoutMS: 30000, // 30 seconds
       socketTimeoutMS: 30000, // 30 seconds
     });
